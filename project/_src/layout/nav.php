3840c01301fabf532f7550f9a91d934b4edbdaac
david <d.marsalone@gmail.com>
2019-09-21 11:10:58 +0200

    nav

--- project/_src/layout/nav.php
+++ project/_src/layout/nav.php
@@ -1,7 +1,8 @@
 <?php
 /** @var \Classiq\Models\Nav $menu */
 $menu=\Classiq\Models\Nav::getByName("menu",true);
-?>
+
+use Pov\Utils\StringUtils; ?>
 <nav id="nav">
 
     <div id="nav-bar" >
@@ -12,14 +13,34 @@ $menu=\Classiq\Models\Nav::getByName("menu",true);
 
     </div>
 
-    <div id="nav-content">
-
-        <div>
-            <?=utils()->string->loremIspum(100,100)?>>
-
-        </div>
-        <div>
-            <?=utils()->string->loremIspum(100,100)?>>
+    <div id="nav-content" class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+                <hr>
+                <?=$menu
+                    ->wysiwyg()
+                    ->field("vars.leftContent_lang")
+                    ->string(StringUtils::FORMAT_HTML)
+                    ->setMediumButtons(["h2","bold","h5","anchor","select-record","removeFormat"])
+                    ->htmlTag("div",true,true)
+                    ->addClass("text-rich");
+                ?>
+            </div>
+            <div class="col-sm-7">
+                <?//films list placeholder?>
+                <hr>
+            </div>
+            <div class="col-sm-2">
+                <hr>
+                <?=$menu
+                    ->wysiwyg()
+                    ->field("vars.rightContent_lang")
+                    ->string(StringUtils::FORMAT_HTML)
+                    ->setMediumButtons(["h2","bold","h5","anchor","select-record","removeFormat"])
+                    ->htmlTag("div",true,true)
+                    ->addClass("text-rich");
+                ?>
+            </div>
         </div>
 
     </div>
