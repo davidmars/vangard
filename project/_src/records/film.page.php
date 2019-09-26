c7efb98d26296dd70aae522e7a158d1488d0c06b
david <d.marsalone@gmail.com>
2019-09-26 08:23:27 +0200

    film page in progress

--- project/_src/records/film.page.php
+++ project/_src/records/film.page.php
@@ -4,22 +4,61 @@ use Classiq\Models\Film;
 the()->htmlLayout()->pageInfo->isHome=false;
 $view->inside("layout/layout",$vv);
 ?>
-<div class="py-big">
+<div class="film-page">
 
         <div class="container">
-            <?=$vv->wysiwyg()
-                ->field("name_lang")
-                ->string(\Pov\Utils\StringUtils::FORMAT_NO_HTML_SINGLE_LINE)
-                ->setDefaultValue($vv->name)
-                ->htmlTag("h1")
-            ?>
+
+            <div class="top">
+                <iframe class="emebed"
+                        src="https://player.vimeo.com/video/32352442?color=ffeb14&portrait=0"
+                        frameborder="0"
+                        allow="autoplay; fullscreen" allowfullscreen>
+                </iframe>
+            </div>
+
+            <div class="body">
+                <?=$vv->wysiwyg()
+                    ->field("name_lang")
+                    ->string(\Pov\Utils\StringUtils::FORMAT_NO_HTML_SINGLE_LINE)
+                    ->setDefaultValue($vv->name)
+                    ->htmlTag("h1")
+                ?>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="text-rich">
+                            <h2>subtitle here baby</h2>
+                            <?=utils()->string->loremIspum(50,150)?>
+                        </div>
+                        <div class="text-rich">
+                            <h2>Credits</h2>
+                            <?=utils()->string->loremIspum(70,100)?>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="text-rich">
+                            <h2>zobi</h2>
+                            <?=utils()->string->loremIspum(50,50)?>
+                        </div>
+                    </div>
+                </div>
+
+                <?=$vv->wysiwyg()->field("blocks")
+                    ->listJson(site()->blocksList)
+                    ->htmlTag()
+                    ->addClass("blocks");
+                ?>
+
+            </div>
+
+
+
+
+
+
+
         </div>
 
-        <?=$vv->wysiwyg()->field("blocks")
-            ->listJson(site()->blocksList)
-            ->htmlTag()
-            ->addClass("blocks");
-        ?>
+
 
 
 
--- project/php-classes/Classiq/Models/Film.php
+++ project/php-classes/Classiq/Models/Film.php
@@ -8,7 +8,8 @@ namespace Classiq\Models;
  * @package Classiq\Models
  *
  * @property $previews Liste des preview vidéo
- * @property $video
+ * @property string $video L'url Vimeo de la video
+ * @property string $subtitle_lang le sous titre
  *
  */
 class Film extends Page
@@ -16,10 +17,15 @@ class Film extends Page
     static $icon="cq-tests-film-play";
 
     /**
+     * Les previews du film
      * @return Preview[]
      */
     public function previews(){
         return Preview::getByUids($this->previews);
     }
+    public function getVideoEmbed(){
+
+    }
+
 
 }
