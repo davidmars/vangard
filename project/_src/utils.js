9e751dfde3b66588f2c43328a554fd7f72740063
david <d.marsalone@gmail.com>
2019-11-01 10:39:58 +0100

    responsive + utils

--- project/_src/utils.js
+++ project/_src/utils.js
@@ -1,4 +1,23 @@
 window.randChar=function(){
     let possible= "abcdefghijklmnoprstuvwxyz!?#";
     return possible.charAt(Math.floor(Math.random() * possible.length));
-}
+};
+/**
+ *
+ * @param {number} inputValue
+ * @param {number} inputMax
+ * @param {number} outputMax
+ * @param {number} inputMin
+ * @param {number} outputMin
+ * @returns {number}
+ */
+window.ratio=function(inputValue, inputMax, outputMax, inputMin=.0, outputMin=.0){
+    let product = (inputValue - inputMin) / (inputMax - inputMin);
+    return ((outputMax - outputMin) * product) + outputMin;
+};
+
+window.isVisible=function(elm) {
+    var rect = elm.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+};
