c18a351cc9c4932c9ab5729268db4a3877ff9a04
david <d.marsalone@gmail.com>
2019-11-01 05:24:23 +0100

    FilmsPreview propriété yetPlayed

--- project/_src/films-list/FilmPreview.js
+++ project/_src/films-list/FilmPreview.js
@@ -1,20 +1,16 @@
 export default class FilmPreview {
     constructor($film){
         let me =this;
+        /**
+         * Tant que false ça veut dire que la video courrante n'a pas été jouée
+         * @type {boolean}
+         */
+        this.yetPlayed=false;
         this.$film=$film;
         this.$previews=this.$film.find(".previews");
         $film.data("obj",this);
-        this.$text=this.$film.find("span")
+        this.$text=this.$film.find("span");
         this.text=this.$text.text();
-        /*
-        setInterval(function(){
-            if(Math.random()>0.9){
-                me.glitch()
-            }
-
-        },1000)
-
-        */
     }
     glitch(){
         let me =this;
@@ -29,18 +25,32 @@ export default class FilmPreview {
     $videos(){
         return this.$film.find("video");
     }
+    /**
+     * Change de vidéo
+     */
     change(){
+        console.log("change");
+        this.yetPlayed=false;
         this.pauseAll();
         let $last=this.$film.find(".preview").last();
         this.$previews.prepend($last);
     }
+
+    /**
+     * Met tout en pause
+     */
     pauseAll(){
         this.$film.addClass("paused");
         this.$videos().each(function(){
             $(this).get(0).pause();
         })
     }
+
+    /**
+     * Joue la video courrante
+     */
     playFirst(){
+        this.yetPlayed=true;
         this.pauseAll();
         this.$film.removeClass("paused");
         this.$videos().first().get(0).play();
